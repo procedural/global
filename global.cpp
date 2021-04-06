f72f1cf1766e96365aeecd0a027dba0354fdb5ca
Constantine Tarasenkov <iamvfx@gmail.com>
2021-04-06 16:31:36 +0300

    Use flat hash map from https://github.com/skarupke/flat_hash_map/blob/2c4687431f978f02a3780e24b8b701d22aa32d9c/flat_hash_map.hpp (via https://twitter.com/SebAaltonen/status/1129727144073093121)

--- global.cpp
+++ global.cpp
@@ -1,14 +1,14 @@
 #if 0
 rm -f libglobal.a global.o 
-c++ -fPIE -c global.cpp
+c++ -std=c++11 -fPIE -fno-exceptions -fno-rtti -c global.cpp
 ar  r libglobal.a global.o
 rm -f             global.o
 exit
 #endif
 
-#include <map>
+#include "flat_hash_map.hpp"
 
-std::map<const char *, void *> __GLOBAL_MAP_8d802133c4cd76b93c9b6922c658bd55b1c9e3fdcedda89ba18afca5a8eb5070;
+ska::flat_hash_map<const char *, void *> __GLOBAL_MAP_8d802133c4cd76b93c9b6922c658bd55b1c9e3fdcedda89ba18afca5a8eb5070;
 
 extern "C" void GlobalSet(const char * key, void * value) {
   __GLOBAL_MAP_8d802133c4cd76b93c9b6922c658bd55b1c9e3fdcedda89ba18afca5a8eb5070[key] = value;
