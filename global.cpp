67c19d8841345d3903a7417447cc46e1ec40c71f
Constantine Tarasenkov <iamvfx@gmail.com>
2021-04-23 20:09:24 +0300

    Key by string value

--- global.cpp
+++ global.cpp
@@ -5,13 +5,16 @@ exit
 #endif
 
 #include "flat_hash_map.hpp"
+#include <string>
 
-ska::flat_hash_map<const char *, void *> __GLOBAL_MAP_8d802133c4cd76b93c9b6922c658bd55b1c9e3fdcedda89ba18afca5a8eb5070;
+ska::flat_hash_map<std::string, void *> __GLOBAL_MAP_8d802133c4cd76b93c9b6922c658bd55b1c9e3fdcedda89ba18afca5a8eb5070;
 
 extern "C" void GlobalSet(const char * key, void * value) {
-  __GLOBAL_MAP_8d802133c4cd76b93c9b6922c658bd55b1c9e3fdcedda89ba18afca5a8eb5070[key] = value;
+  std::string keyString = key;
+  __GLOBAL_MAP_8d802133c4cd76b93c9b6922c658bd55b1c9e3fdcedda89ba18afca5a8eb5070[keyString] = value;
 }
 
 extern "C" void * GlobalGet(const char * key) {
-  return __GLOBAL_MAP_8d802133c4cd76b93c9b6922c658bd55b1c9e3fdcedda89ba18afca5a8eb5070[key];
+  std::string keyString = key;
+  return __GLOBAL_MAP_8d802133c4cd76b93c9b6922c658bd55b1c9e3fdcedda89ba18afca5a8eb5070[keyString];
 }
